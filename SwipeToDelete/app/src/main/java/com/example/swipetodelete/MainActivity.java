39c89c13603f1267571619abb71748ce9849f583
kaid931103 <48038653+kaid931103@users.noreply.github.com>
2019-06-05 02:03:42 +0800

    0605
    0605

--- SwipeToDelete/app/src/main/java/com/example/swipetodelete/MainActivity.java
+++ SwipeToDelete/app/src/main/java/com/example/swipetodelete/MainActivity.java
@@ -5,6 +5,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
@@ -18,7 +19,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,41 +32,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> stringArrayList = new ArrayList<>();
     CoordinatorLayout coordinatorLayout;
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent intent=new Intent(MainActivity.this,Setting.class);
-            startActivity(intent);
-            return true;
-        }
-        if (id == R.id.action_sort) {
-            Intent intent=new Intent(MainActivity.this,Sort.class);
-            startActivity(intent);
-            return true;
-        }
-        if (id == R.id.action_add) {
-            Intent intent=new Intent(MainActivity.this,Add.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getPermissionsRECORD_AUDIO();
-        //
-
 
 
 
@@ -86,13 +58,52 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void getPermissionsRECORD_AUDIO() {
-       if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
-        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent intent1 = new Intent(MainActivity.this,Setting.class);
+                startActivity(intent1);
+            return true;
+            case R.id.action_sort:
+                Intent intent2 = new Intent(MainActivity.this, Sort.class);
+                startActivity(intent2);
+                return true;
+            case R.id.action_add:
+                    Intent intent3=new Intent(MainActivity.this,Add.class);
+                    startActivity(intent3);
+                    return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+      /*  int id = item.getItemId();
+        if (id == R.id.action_settings) {
+
+            Intent intent1 = new Intent(MainActivity.this, Sort.class);
+            startActivity(intent1);
+            return true;
+        }
+            if (id == R.id.action_sort) {
+                Intent intent2 = new Intent(MainActivity.this, Sort.class);
+                startActivity(intent2);
+                return true;
+            }
+          if (id == R.id.action_add) {
+            Intent intent3=new Intent(MainActivity.this,Add.class);
+            startActivity(intent3);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+*/
     private void populateRecyclerView() {
         stringArrayList.add("MilkShop");
         stringArrayList.add("CoCo");
