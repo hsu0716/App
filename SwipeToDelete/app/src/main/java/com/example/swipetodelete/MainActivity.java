22535b94fc70e28a7f708c841f3244b034c8aefc
hsu0716 <a.99line@gmail.com>
2019-06-05 00:27:30 +0800

    a
    aa

---
+++ SwipeToDelete/app/src/main/java/com/example/swipetodelete/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.swipetodelete;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter mAdapter;
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    CoordinatorLayout coordinatorLayout;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent=new Intent(MainActivity.this,Setting.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_sort) {
+            Intent intent=new Intent(MainActivity.this,Sort.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_add) {
+            Intent intent=new Intent(MainActivity.this,Add.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getPermissionsRECORD_AUDIO();
+        //
+
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+
+        populateRecyclerView();
+        enableSwipeToDeleteAndUndo();
+
+
+    }
+    public void getPermissionsRECORD_AUDIO() {
+       if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+    }
+
+    private void populateRecyclerView() {
+        stringArrayList.add("MilkShop");
+        stringArrayList.add("CoCo");
+        stringArrayList.add("DaYung's");
+        stringArrayList.add("Fifty Lan");
+        stringArrayList.add("TigerSugar");
+        stringArrayList.add("Queenny");
+        stringArrayList.add("Joly");
+
+
+        mAdapter = new RecyclerViewAdapter(stringArrayList);
+        recyclerView.setAdapter(mAdapter);
+
+
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+
+                final int position = viewHolder.getAdapterPosition();
+                final String item = mAdapter.getData().get(position);
+
+                mAdapter.removeItem(position);
+
+
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Item was removed from the list.", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        mAdapter.restoreItem(item, position);
+                        recyclerView.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        };
+
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+    }
+
+
+}
+
+
